200d72f89eb9821e725f74af24c887727b991b0a
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2024-12-02 11:38:10 +0530

    BLoC transition complete with edit mode working.

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -5,27 +5,39 @@ import 'package:tasky/models/task.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import '../cubit/task_cubit.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({super.key, required this.task});
-
   final Task task;
 
   @override
-  Widget build(BuildContext context) {
-    String newTitle = task.taskTitle;
-    bool editMode = newTitle.isEmpty ? true : false;
+  State<TaskTile> createState() => _TaskTileState();
+}
 
+class _TaskTileState extends State<TaskTile> {
+  late String newTitle;
+  late bool editMode;
+
+  @override
+  void initState() {
+    newTitle = widget.task.taskTitle;
+    editMode = newTitle.isEmpty ? true : false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
-      onDoubleTap: () => editMode = !editMode,
+      onDoubleTap: () => setState(() {
+        editMode = !editMode;
+      }),
       onTap: () => BlocProvider.of<TaskCubit>(context).updateTask(
-        task,
-        task.copyWith(isTaskDone: !task.isTaskDone),
+        widget.task,
+        widget.task.copyWith(isTaskDone: !widget.task.isTaskDone),
       ),
       child: Slidable(
         endActionPane: ActionPane(motion: const StretchMotion(), children: [
           SlidableAction(
-            onPressed: (context) =>
-                BlocProvider.of<TaskCubit>(context).removeTaskByObject(task),
+            onPressed: (context) => BlocProvider.of<TaskCubit>(context)
+                .removeTaskByObject(widget.task),
             backgroundColor: AppColors.background,
             foregroundColor: AppColors.circleAvatarBackground,
             icon: Icons.delete,
@@ -36,29 +48,30 @@ class TaskTile extends StatelessWidget {
           title: editMode
               ? TextFormField(
                   style: const TextStyle(color: AppColors.taskText),
-                  initialValue: task.taskTitle,
+                  initialValue: widget.task.taskTitle,
                   autofocus: true,
                   onChanged: (value) => newTitle = value,
-                  onTapOutside: (value) {
-                    // String title = newTitle.trim();
+                  onTapOutside: (value) => setState(() {
                     if (newTitle.isNotEmpty) {
+                      BlocProvider.of<TaskCubit>(context).updateTask(
+                          widget.task,
+                          widget.task.copyWith(taskTitle: newTitle));
+                    } else if (widget.task.taskTitle.trim().isEmpty) {
                       BlocProvider.of<TaskCubit>(context)
-                          .updateTask(task, task.copyWith(taskTitle: newTitle));
-                    } else if (task.taskTitle.trim().isEmpty) {
-                      BlocProvider.of<TaskCubit>(context)
-                          .removeTaskByObject(task);
+                          .removeTaskByObject(widget.task);
                     }
                     editMode = !editMode;
-                  },
+                  }),
                 )
               : Text(
-                  task.taskTitle,
+                  widget.task.taskTitle,
                   style: TextStyle(
-                    color: task.isTaskDone
+                    color: widget.task.isTaskDone
                         ? AppColors.completedTaskText
                         : AppColors.taskText,
-                    decoration:
-                        task.isTaskDone ? TextDecoration.lineThrough : null,
+                    decoration: widget.task.isTaskDone
+                        ? TextDecoration.lineThrough
+                        : null,
                   ),
                 ),
         ),
